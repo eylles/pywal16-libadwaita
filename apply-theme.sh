062d3917be458b3a907726dc81b36b7a99aa64ce
eylles <ed.ylles1997@gmail.com>
2024-02-24 14:00:00 -0600

    added: apply-theme.sh script

---
+++ apply-theme.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+myname="${0##*/}"
+
+gradience_dir="${HOME}/.var/app/com.github.GradienceTeam.Gradience/config/presets/user"
+kvantum_dir="${XDG_CONFIG_HOME:-~/.config}/Kvantum"
+
+wal_template_dir="${XDG_CACHE_HOME:-~/.cache}/wal"
+
+# template name
+# default: pywal
+template=pywal
+
+# theme name
+# default: pywal
+theme=pywal
+
+settheme=0
+
+# copy gradience theme
+cp -f "$wal_template_dir/${template}.json" "${gradience_dir}/${theme}.json"
+
+_help () {
+    printf 'usage: %s [OPTION]\n' "$myname"
+    printf 'options:\n'
+    printf '\t-n theme\tname for theme if not defalt. default: pywal\n'
+    printf '\t-t template\tname for template if not default. default: pywal\n'
+    printf '\t-s \t\tset themes with gradience and kvantum\n'
+    printf '\t\t\tthis assumes gradience is installed through flatpak\n'
+}
+
+
+while getopts "n:t:sh" opt; do case "${opt}" in
+  n)
+    theme="$OPTARG"
+    ;;
+  t)
+    template="$OPTARG"
+    ;;
+  s)
+    settheme=1
+    ;;
+  h)
+    _help
+    exit 0
+    ;;
+  *)
+    printf '%s: invalid option %s\n' "$myname" "$opt" >&2
+    _help
+    exit 1
+    ;;
+esac done
+
+
+# kvantum theme dir
+kvtheme_dir="$kvantum_dir/${theme}"
+
+# check if kvantum theme directory exists
+if [ -d "$kvtheme_dir" ]; then
+  mkdir -p "$kvtheme_dir"
+fi
+
+# copy kvantum theme
+cp -f "$wal_template_dir/${template}.kvconfig" "$kvtheme_dir/${theme}.kvconfig"
+cp -f "$wal_template_dir/${template}.svg" "$kvtheme_dir/${theme}.svg"
+
+# do we actually set the themes?
+if [ "$settheme" -eq 1 ]; then
+  flatpak run --command=gradience-cli com.github.GradienceTeam.Gradience apply -n "$theme" --gtk both
+  kvantummanager --set "$theme"
+fi
