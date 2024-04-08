c8227bf4718f2fc948092125bcb3cccf6215c65a
eylles <ed.ylles1997@gmail.com>
2024-04-08 16:05:00 -0600

    actually write file

--- scripts/build-gradience.py
+++ scripts/build-gradience.py
@@ -137,14 +137,95 @@ gradience_theme = {
         "dialog_fg_color": col["col15"],
         "popover_bg_color": col["col0"],
         "popover_fg_color": col["col15"],
-        "shade_color": col["col8"],
+        "shade_color": lighten_color(col["col0"], 3),
         "scrollbar_outline_color": col["col12"],
         "sidebar_bg_color": col["col0"],
         "sidebar_fg_color": col["col15"],
         "sidebar_backdrop_color": lighten_color(col["col0"], 1),
-        "sidebar_shade_color": lighten_color(col["col0"], 2)
-    }
+        "sidebar_shade_color": lighten_color(col["col0"], 2),
+    },
+    "palette": {
+        "blue_": {
+            "1": "#99c1f1",
+            "2": "#62a0ea",
+            "3": "#3584e4",
+            "4": "#1c71d8",
+            "5": "#1a5fb4"
+        },
+        "green_": {
+            "1": "#8ff0a4",
+            "2": "#57e389",
+            "3": "#33d17a",
+            "4": "#2ec27e",
+            "5": "#26a269"
+        },
+        "yellow_": {
+            "1": "#f9f06b",
+            "2": "#f8e45c",
+            "3": "#f6d32d",
+            "4": "#f5c211",
+            "5": "#e5a50a"
+        },
+        "orange_": {
+            "1": "#ffbe6f",
+            "2": "#ffa348",
+            "3": "#ff7800",
+            "4": "#e66100",
+            "5": "#c64600"
+        },
+        "red_": {
+            "1": "#f66151",
+            "2": "#ed333b",
+            "3": "#e01b24",
+            "4": "#c01c28",
+            "5": "#a51d2d"
+        },
+        "purple_": {
+            "1": "#dc8add",
+            "2": "#c061cb",
+            "3": "#9141ac",
+            "4": "#813d9c",
+            "5": "#613583"
+        },
+        "brown_": {
+            "1": "#cdab8f",
+            "2": "#b5835a",
+            "3": "#986a44",
+            "4": "#865e3c",
+            "5": "#63452c"
+        },
+        "light_": {
+            "1": "#ffffff",
+            "2": "#f6f5f4",
+            "3": "#deddda",
+            "4": "#c0bfbc",
+            "5": "#9a9996"
+        },
+        "dark_": {
+            "1": "#77767b",
+            "2": "#5e5c64",
+            "3": "#3d3846",
+            "4": "#241f31",
+            "5": "#000000"
+        }
+    },
+    "custom_css": {
+        "gtk4": "",
+        "gtk3": ""
+    },
+    "plugins": {}
 }
 
-for keys, values in gradience_theme["variables"].items():
-    print("{key: >24}: {value}".format(key=keys, value=values))
+# for keys, values in gradience_theme["variables"].items():
+#     print("{key: >24}: {value}".format(key=keys, value=values))
+
+home_dir = os.environ.get('HOME', '/home/{}'.format(username))
+gr_dir = ".var/app/com.github.GradienceTeam.Gradience/config/presets/user"
+out_dir = "{}/{}".format(home_dir, gr_dir)
+theme_file = "pywal.json"
+o_file = "{d}/{f}".format(d=out_dir, f=theme_file)
+
+# print(o_file)
+
+with open(o_file, "w") as outfile:
+    json.dump(gradience_theme, outfile, indent=4)
