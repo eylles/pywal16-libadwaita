c74a770bf9e9a8339bd0a3297e83cdf6a9c29d20
eylles <ed.ylles1997@gmail.com>
2024-12-06 22:08:13 -0600

    Actually write the gtk.css

--- scripts/gradience2css.py
+++ scripts/gradience2css.py
@@ -58,6 +58,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", required="true", help="JSON file to use")
 parser.add_argument("-d", "--debug", action='store_true',
                     help="Show Debug Output")
+parser.add_argument(
+    "-t",
+    "--target",
+    dest="targ",
+    help="should be the PARENT dir "
+    "containing the 'gtk-3.0' and 'gtk-4.0' dirs "
+    "otherwise the target will default to: "
+    "$XDG_CONFIG_HOME",
+)
+
 
 # Read arguments from command line
 args = parser.parse_args()
@@ -127,5 +137,41 @@ if "custom_css" in data:
     gtk3 = gtk3 + "\n" + data["custom_css"]["gtk3"]
     gtk4 = gtk4 + "\n" + data["custom_css"]["gtk4"]
 
-print(gtk3)
-print(gtk4)
+if args.debug:
+    print("gtk3 stylesheet:")
+    print(gtk3)
+    print("")
+    print("gtk4 stylesheet:")
+    print(gtk4)
+    print("")
+
+if args.targ:
+    target_dir = "{}".format(args.targ)
+else:
+    username = os.environ["USER"]
+    home_dir = os.environ.get("HOME", "/home/{}".format(username))
+    xdg_conf = os.environ.get("XDG_CONFIG_HOME", "{}/.config".format(home_dir))
+    target_dir = "{}".format(xdg_conf)
+    # print(target_dir)
+
+target_gtk3 = "{}/gtk-3.0".format(target_dir)
+# print(target_gtk3)
+target_gtk4 = "{}/gtk-4.0".format(target_dir)
+# print(target_gtk4)
+
+
+def write_css(output_dir, output_string):
+    if os.path.exists(output_dir):
+        if args.debug:
+            print("dir {} exists".format(output_dir))
+    else:
+        if args.debug:
+            print("dir {} does not exist, creating it".format(output_dir))
+        os.mkdir(output_dir)
+    out_file = "{}/gtk.css".format(output_dir)
+    with open(out_file, "w") as of:
+        of.write(output_string)
+
+
+write_css(target_gtk3, gtk3)
+write_css(target_gtk4, gtk4)
